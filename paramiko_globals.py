a1950e7ee6b497a7143872d704d466f225b44f3a
Muralidharan R <techwithmurali@gmail.com>
2022-03-03 21:27:44 +0530

    Add files via upload

---
+++ paramiko_globals.py
@@ -0,0 +1,10 @@
+import os
+hostname="whf00oty.in.oracle.com"
+username="username"
+password="password"
+localpath  = r'C:\WorkDir\FolderName'
+serverpath = '/scratch/work_area/Murali/ServerPath'
+lst_Files = ['File1.html','File2.html']
+localpath_dest =  r'C:\WorkDir\DestFolder'
+output_file = os.path.join(localpath_dest,'outfile.txt')
+ 
